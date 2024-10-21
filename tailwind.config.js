9eb8dc4f8009cab39a3ebb539f561a8c13bf8b16
Huzaifa Zahoor <huzaifazahoor654@gmail.com>
2024-10-21 22:11:19 +0500

    set the local tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    './index.html',
+    './style.js',
+    './popup.js',
+    './style.css',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3498db',
+        secondary: '#2c3e50',
+        dark: '#1a202c',
+        light: '#2d3748',
+      },
+    },
+  },
+  plugins: [],
+}
